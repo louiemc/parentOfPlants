650cb961634b2cf7a4d65b771d71d597c8133060
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-08 10:54:25 -0400

    adding homepage route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
+// const apiRoutes = require("./api");
 
 // const app = express();
 
@@ -10,18 +10,23 @@ const apiRoutes = require("./api");
 
 // DEFINE ROUTES
 // API Routes
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 
 // IF NO API ROUTES ARE HIT, SEND THE REACT APP
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"))
-});
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, "../../client/build/index.html"))
+// });
 
 // OTHER ROUTES
 // HomePage Route
 // app.post("/api/users", (req, res) => {
 //   let name = req.body.name;
 // })
+
+// GET HOME PAGE
+router.get('/', (req, res, next) => {
+  res.json("Homepage");
+});
 
 
 module.exports = router;

f8d36bf0f042a34cb53e48255c7762b2527c03cd
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-08 18:00:11 -0400

    Routes and Components added

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     username: "",
     useremail: "",
     isSwitched: false,
-    guest false
+    guest: false
   };
 
   changeLogStatus = () => {
@@ -36,13 +36,13 @@ class App extends Component {
     if (this.state.isSignedIn !== prevState.isSignedIn) {
       console.log("state changed to: " + this.state.isSignedIn);
     }
-  };
+  }
 
   signAsGuest = () => {
     var userGuest = this.state.guest;
     userGuest = !userGuest;
     this.setState({ guest: userGuest });
-    console.log('did the user opt to sign in as a guest? ' + userGuest);
+    console.log("did the user opt to sign in as a guest? " + userGuest);
   };
 
   render() {
@@ -50,9 +50,60 @@ class App extends Component {
     console.log("<APP /> component: " + isSignedIn);
     return (
       <div className="App">
-        
+        <Router>
+          <div>
+            <div className="app-header">
+              <div className="phood-logo" />
+              <Navbar
+                logOut={this.logOut}
+                guest={guest}
+                signAsGuest={this.signAsGuest}
+              />
+            </div>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Home
+                    {...props}
+                    isSignedIn={isSignedIn}
+                    changeLogStatus={this.changeLogStatus}
+                    signAsGuest={this.signAsGuest}
+                    guest={guest}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/profile"
+                render={props => (
+                  <Profile
+                    {...props}
+                    isSignedIn={isSignedIn}
+                    changeLogStatus={this.changeLogStatus}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/plants"
+                render={props => (
+                  <Plants
+                    {...props}
+                    isSignedIn={isSignedIn}
+                    changeLogStatus={this.changeLogStatus}
+                    handleSwitch={this.handleSwitch}
+                    signAsGuest={this.signAsGuest}
+                    guest={guest}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </Router>
       </div>
-    )
+    );
   }
 }
 

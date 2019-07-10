a39738d779b4bd6fa1d8125f2bac18d14c40afaa
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-09 23:12:26 -0400

    home page header and description

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -14,18 +14,24 @@ class Home extends Component {
     this.setState({ open: true });
   };
 
-  handleClose =e => {
+  handleClose = e => {
     this.setState({ open: false });
   };
 
   render() {
     return (
       <div>
-        
+        <h1 className="siteName">Plant Parenthood</h1>
+
+        <p id="siteDescription">
+          The ups and downs of being a plant parent. Maybe you're new to plant
+          parenthood? Maybe you're an experienced plant parent? We care about
+          your time and learning experience. We experiment with plant care and
+          propagation. Plant Parenthood is an app that provides you with plant
+          care instructions, tips and tricks, and propagation tips that let you
+          grow as a plant parent.
+        </p>
       </div>
-    )
+    );
   }
-
-
-
 }
